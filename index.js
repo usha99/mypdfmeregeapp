fb978b60a01e6ef7510d0f617e6be0e27a4701c7
usha <ushagupta97895@gmail.com>
2023-07-21 11:15:13 +0530

    first commit

---
+++ index.js
@@ -0,0 +1,26 @@
+const express = require("express");
+const path = require("path");
+const app = express();
+const multer = require("multer");
+const upload = multer({ dest: "uploads/" });
+const port = 3000;
+const { mergedPdf } = require("./testpdfs");
+app.use("/static", express.static("public"));
+
+app.get("/", (req, res) => {
+  res.sendFile(path.join(__dirname, "templates/index.html"));
+});
+
+app.post("/merge", upload.array("pdfs", 12), async (req, res, next) => {
+  console.log(req.files);
+  await mergedPdf(
+    path.join(__dirname, req.files[0].path),
+    path.join(__dirname, req.files[1].path)
+  );
+  res.redirect("http://localhost:3000/static/merged.pdf");
+  //   res.send({ data: req.files });
+});
+
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`);
+});
---
+++ testpdfs.js
@@ -0,0 +1,22 @@
+const PDFMerger = require("pdf-merger-js");
+
+var merger = new PDFMerger();
+
+// const mergedPdf = (async (p1, p2) => {
+//   await merger.add("1.pdf"); //merge all pages. parameter is the path to file and filename.
+//   await merger.add("2.pdf"); // merge only page 2
+
+//   await merger.save("public/merged.pdf"); //save under given name and reset the internal document
+// })();
+
+// module.exports = { mergedPdf };
+const mergedPdf = async (p1, p2) => {
+  var merger = new PDFMerger();
+  await merger.add(p1); // Merge all pages from p1
+  await merger.add(p2); // Merge only page 2 from p2
+  await merger.save("public/merged.pdf"); // Save under the given name and reset the internal document
+  return merger; // Return the merger instance
+};
+
+// Export the mergedPdf function
+module.exports = { mergedPdf };
