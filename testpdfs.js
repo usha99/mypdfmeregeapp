e95135ff596dd0cd2c6757d91d1c472777c899c4
usha <ushagupta97895@gmail.com>
2023-07-24 13:04:33 +0530

    changes

--- testpdfs.js
+++ testpdfs.js
@@ -2,22 +2,14 @@ const PDFMerger = require("pdf-merger-js");
 
 var merger = new PDFMerger();
 
-// const mergedPdf = (async (p1, p2) => {
-//   await merger.add("1.pdf"); //merge all pages. parameter is the path to file and filename.
-//   await merger.add("2.pdf"); // merge only page 2
-
-//   await merger.save("public/merged.pdf"); //save under given name and reset the internal document
-// })();
-
-// module.exports = { mergedPdf };
 const mergedPdf = async (p1, p2) => {
   var merger = new PDFMerger();
-  await merger.add(p1); // Merge all pages from p1
-  await merger.add(p2); // Merge only page 2 from p2
+  await merger.add(p1);
+  await merger.add(p2);
   let d = new Date().getTime();
   await merger.save(`public/${d}.pdf`);
-  return d; // Save under the given name and reset the internal document
-  return merger; // Return the merger instance
+  return d;
+  return merger;
 };
 
 // Export the mergedPdf function
